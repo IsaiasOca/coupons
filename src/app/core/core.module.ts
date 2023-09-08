55ded96ef74e8ea99aa176a92196973ca5a1aa2e
Isaias Ocampo <30940023+IsaiasOca@users.noreply.github.com>
2023-09-08 12:09:34 -0600

    Add interceptors to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRequestInterceptor } from './interceptors/http-request.interceptor';
+import { HttpResponseInterceptor } from './interceptors/http-response.interceptor';
 
 
 
@@ -7,6 +10,18 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],  
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpResponseInterceptor,
+      multi: true,
+    }
+  ],
 })
 export class CoreModule { }
